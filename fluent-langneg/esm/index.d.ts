bdabb0ab668f6b950ae1d2db3dee030d95de8cfb
Sirui Li <sirui.li@ef.com>
2021-08-03 17:47:17 +0100

    Add compiled module

---
+++ fluent-langneg/esm/index.d.ts
@@ -0,0 +1,3 @@
+export { negotiateLanguages, NegotiateLanguagesOptions } from "./negotiate_languages";
+export { acceptedLanguages } from "./accepted_languages";
+export { filterMatches } from "./matches";
---
+++ fluent-langneg/esm/index.js
@@ -0,0 +1,11 @@
+/*
+ * @module fluent-langneg
+ * @overview
+ *
+ * `fluent-langneg` provides language negotiation API that fits into
+ * Project Fluent localization composition and fallbacking strategy.
+ *
+ */
+export { negotiateLanguages } from "./negotiate_languages";
+export { acceptedLanguages } from "./accepted_languages";
+export { filterMatches } from "./matches";
